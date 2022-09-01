93a6e052ce9fc1298c602457235c2483af161956
VorobyovR <vorobyovromanmail@gmail.com>
2022-09-01 15:13:32 +0300

    data types and variable

--- L1/1.py
+++ L1/1.py
@@ -1,1 +1,43 @@
-print("Hello world!")
+from xmlrpc.client import boolean
+
+
+# print("Hello world!")
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+value = None
+a = 123
+b =1.23
+print(a)
+print(b)
+value = 1234
+print(value)
+print(type(a))
+print(type(b))
+print(type(value))
+
+s = 'Hello \n"world"' #\n перевод на новую строку
+print(s)
+s1 = 'Hello "world"' #\n перевод на новую строку
+
+
+# интерпаляция
+print(a, ' - ', b, ' - ', s1)
+print('{} - {} - {}'.format(a,b,s1))
+print('{1} - {2} - {0}'.format(a,b,s1)) #переставлять местами
+print(f'{a} - {b} - {s1}')
+
+f = True
+print(f)
+print(type(f))
+
+
+f = False
+
+for item in range(3):
+    print(item, end=" ")
+
+else:
+    f = True
+print(f" цикл выполнен полностью {f}")
