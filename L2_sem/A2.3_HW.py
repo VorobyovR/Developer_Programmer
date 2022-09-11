aee43fc3f6cc7587f4cc01904343666c8447dacd
VorobyovR <vorobyovromanmail@gmail.com>
2022-09-11 17:10:50 +0300

    L2_sem corr 3

--- L2_sem/A2.3_HW.py
+++ L2_sem/A2.3_HW.py
@@ -1,20 +1,17 @@
 from os import system
 system('cls')
 
-# 3. Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-# Пример:
-# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+# 3. Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+
 
 num = int(input('Input a number: '))
-multiplication = 1
 result = 0
 sum = 0
-list = []
-for i in range(num):
-    i+=1
-    multiplication*=i
-    list.append(multiplication)
-for i in list:
-    result = (1+1/i)**i
+d = {}
+
+for i in range(1, num+1):
+    result = round((1+1/i)**i, 2)
     sum = sum + result
-print(f'For num = {6}: {round(sum)}')
+    d[i] = result
+print(f'For num = {num}: {round(sum)}')
+print(d)
